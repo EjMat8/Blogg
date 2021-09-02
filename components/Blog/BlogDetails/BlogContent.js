24ca5961f03bf4c4ee98bcbf0c5b6bb847cb2494
EjMat8 <franzelijahmatugas@gmail.com>
2021-09-02 20:14:13 +0800

    fixed snippet

--- components/Blog/BlogDetails/BlogContent.js
+++ components/Blog/BlogDetails/BlogContent.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
 import { Box, Center, Text, VStack, Heading } from "@chakra-ui/react";
 
